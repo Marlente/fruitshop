34a95b146159e8d22e0397149932fe08c8612ebe
Marlente <marlo-k@gmx.de>
2022-07-23 18:08:27 +0800

    created cart state component as well as UI

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const fruits = [{name: "Mango", price: 42, picture: "https://images.immediate.co.uk/production/volatile/sites/30/2018/08/mango-fee0d79-e1648560084294.jpg?quality=90&resize=768,574"}, 
 {name: "Banana", price: 21, picture: "https://img.freepik.com/free-vector/vector-ripe-yellow-banana-bunch-isolated-white-background_1284-45456.jpg"}]
 
@@ -18,11 +20,38 @@ function Shelf ({fruits}){
   </div>)
 }
 
+function Cart ({cart}){
+
+  return (
+    <div className="p-10 m-5 border-2 border-gray-700 rounded-lg">
+      <h1 className="text-xl font-bold">Cart</h1>
+      {Object.keys(cart).map((itemId) => {
+        return (<div>
+          {itemId}
+          {cart[itemId]}
+        </div>
+          )
+      })}
+    </div>
+  )
+
+}
+
+
 
 export default function Home() {
-
+  const [cart, setCart] = useState({
+      Mango:0, Banana:0
+  })
+  const addItem = (itemId) => {
+    setCart({...cart, [itemId]: cart[itemId] + 1})
+  }
+  const removeItem = (itemId) => {
+    setCart({...cart, [itemId]: cart[itemId] - 1})
+  }
   return (
   <div> 
     <Shelf fruits={fruits} /> 
+    <Cart cart={cart}/> 
   </div>)
 }
