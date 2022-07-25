921a87cfd758ae48e6a1d63620cac66ba78c03ca
Marlente <marlo-k@gmx.de>
2022-07-25 11:13:46 +0800

    initialized a total price button and styled the page with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,32 @@
 import { useState } from "react"
 
-const fruits = [{name: "Mango", price: 42, picture: "https://images.immediate.co.uk/production/volatile/sites/30/2018/08/mango-fee0d79-e1648560084294.jpg?quality=90&resize=768,574"}, 
-{name: "Banana", price: 21, picture: "https://img.freepik.com/free-vector/vector-ripe-yellow-banana-bunch-isolated-white-background_1284-45456.jpg"}]
+const fruits = [
+  {name: "Mangosteen", price: 19, picture: "https://a.cdn-hotels.com/gdcs/production165/d1432/066a2dfb-de38-40d3-812b-6cf81c64341f.jpg"}, 
+  {name: "Banana", price: 23, picture: "https://img.freepik.com/free-vector/vector-ripe-yellow-banana-bunch-isolated-white-background_1284-45456.jpg"},
+  {name: "Pineapple", price: 39, picture: "https://static.libertyprim.com/files/familles/ananas-large.jpg?1569271716"},
+  {name: "Dragonfruit", price: 34, picture: "https://cdn.shopify.com/s/files/1/0600/5650/2485/products/dragonfruit_red-1561282572637.jpg?v=1646225933&width=1946"},
+  {name: "Lychee", price: 7, picture: "https://images.squarespace-cdn.com/content/v1/5d9aba8504a05a7cc222de1b/1577861273128-OG39CJ8SL7AISCIP1Z08/rambutan-inside.jpg?format=1000w"},
+  {name: "SalakBali", price: 12, picture: "https://visitbali.id/images/data/2020/Feb/25/5e54d6b01859cf47d.jpeg"},
+]
 
 
 
 
 function Shelf ({fruits, onAddToCart}){
+
   return (<div>
+
+    <div className="p-10 w-full bg-blue-100">
+      <h1 className="text-3xl font-bold">Marlo's Fruitshop</h1>
+      <h3 className="text-xl italic">As fresh as you can get them</h3>
+    </div>
+
     {fruits.map((fruit) => {
-      return (<div className="p-10 m-5 border-2 border-gray-700 rounded-lg inline-block"> 
+      return (<div className="p-10 m-4 border-2 border-gray-700 rounded-lg inline-block"> 
         <div className="text-2xl font-bold">{fruit.name}</div>
-        <div className="text-lg">Price: {fruit.price}</div>
-        <div className="w-80 h-80 bg-cover" style={{backgroundImage: `url(${fruit.picture})`}}></div>
-        <button className="p-2 m-2 bg-blue-500 rounded-lg text-white" onClick={() => {
+        <div className="text-lg">Price: {fruit.price}$</div>
+        <div className="w-80 h-80 bg-cover mt-2" style={{backgroundImage: `url(${fruit.picture})`}}></div>
+        <button className="p-2 mt-4 bg-blue-500 rounded-lg text-white" onClick={() => {
           onAddToCart(fruit.name)
         }}>Add to cart</button>
       </div>
@@ -22,32 +35,34 @@ function Shelf ({fruits, onAddToCart}){
   </div>)
 }
 
-function Cart ({cart, onAddToCart, onRemoveFromCart, onRemoveAllItems}){
-
+function Cart ({cart, onAddToCart, onRemoveFromCart, onRemoveAllItems, onGetTotalPrice}){
+  let totalPrice = 0
+  totalPrice = onGetTotalPrice()
   return (
-    <div className="p-10 m-5 border-2 border-gray-700 rounded-lg">
-      <h1 className="text-xl font-bold">Cart</h1>
+    <div className="p-10 m-4 border-2 border-gray-700 rounded-lg">
+      <h1 className="text-2xl font-bold">Shoppingcart</h1>
       {Object.keys(cart).map((itemId) => {
         return (<div>
-          <div>
-            {itemId}
-            {cart[itemId]}
+          <div className="flex">
+            <p className="pr-4 font-xl">{itemId}</p>
+            <p className="pr-2">Amount:</p>
+            <p>{cart[itemId]}</p>
           </div>
           <div className="flex">
           <button className="p-2 m-2 bg-blue-500 rounded-lg text-white" onClick={() => {
-          onAddToCart(itemId)
+          onAddToCart(itemId), totalPrice = onGetTotalPrice()
         }}>+1</button>
         <button className="p-2 m-2 bg-blue-500 rounded-lg text-white" onClick={() => {
-          onRemoveFromCart(itemId)
+          onRemoveFromCart(itemId), totalPrice = onGetTotalPrice()
         }}>-1</button>
         <button className="p-2 m-2 bg-blue-500 rounded-lg text-white" onClick={() => {
-          onRemoveAllItems(itemId)
+          onRemoveAllItems(itemId), totalPrice = onGetTotalPrice()
         }}>Remove All</button>
           </div>
-          
         </div>
           )
       })}
+      <p className="p-2 m-2 bg-blue-500 rounded-lg text-white">Total price: {totalPrice.toString()}$</p>
     </div>
   )
 
@@ -57,7 +72,7 @@ function Cart ({cart, onAddToCart, onRemoveFromCart, onRemoveAllItems}){
 
 export default function Home() {
   const [cart, setCart] = useState({
-      Mango:0, Banana:0
+      Mangosteen:0, Banana:0, Pineapple:0, Dragonfruit:0, Lychee:0, SalakBali:0
   })
   const addItem = (itemId) => {
     setCart({...cart, [itemId]: cart[itemId] + 1})
@@ -68,6 +83,17 @@ export default function Home() {
   const removeAllItems = (itemId) => {
     setCart({...cart, [itemId]: 0})
   }
+  const totalPrice  = () => {
+    let priceFruits = 0
+    for(let k in fruits){
+      console.log(priceFruits)
+      console.log(cart[fruits[k].name])
+      console.log(fruits[k].price)
+      priceFruits += cart[fruits[k].name] * fruits[k].price
+        }
+    console.log(priceFruits)
+    return priceFruits
+  }
 
   return (
   <div> 
@@ -76,10 +102,11 @@ export default function Home() {
     onAddToCart={addItem}
     /> 
     <Cart 
-    cart={cart} 
+    cart={cart}
     onAddToCart={addItem} 
     onRemoveFromCart={removeItem}
     onRemoveAllItems={removeAllItems}
+    onGetTotalPrice={totalPrice}
     />
   </div>)
 }
